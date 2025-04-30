3776860f1d558a59618d3c7e17026ef6fae73627
Daniel Olivares <142864856+enemixee@users.noreply.github.com>
2025-04-29 21:01:08 -0600

    Delete Ignore directory

--- Ignore/boton.js
+++
@@ -1,6 +0,0 @@
-function changeContent(section) {
-    let contents = document.querySelectorAll('.content');
-    contents.forEach(div => div.classList.remove('active'));
-    
-    document.getElementById(section).classList.add('active');
-}
