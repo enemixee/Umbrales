529ea5f8fa95488748112bc7c5ab47b429135230
Daniel Olivares <142864856+enemixee@users.noreply.github.com>
2025-07-06 20:43:54 -0600

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-    var menuList = document.getElementById('menu-list');
-    if (menuList.style.display === 'block') {
-        menuList.style.display = 'none';
-    } else {
-        menuList.style.display = 'block';
-        document.body.style.overflowY = 'hidden';
-    }
-}
